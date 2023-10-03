f9cb6a59c117daec6c4837a1a955e17d990e2f63
Musakalamz <ogunsolumusa@gmail.com>
2023-10-03 12:14:11 -0700

    islower
    a function that checks for lowercase character.

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# File: 7-islower.py
+# Auth: Musa_kalamz A Ogunsolu
+
+""" checks for lowercase character """
+
+def islower(c):
+    """Check for lowercase characters."""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
