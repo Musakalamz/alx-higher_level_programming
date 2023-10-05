da2a51df282052e008031231b2d3f887d19b3b46
Musakalamz <ogunsolumusa@gmail.com>
2023-10-05 09:38:52 -0700

    Everything can be imported
    a program that imports the variable a from the file variable_load_5.py and prints its value.

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# File: 5-variable_load.py
+# Auth: Musa_kalamz A Ogunsolu
+
+if __name__ == "__main__":
+    """Prints the value of variable "a" from variable_load_5."""
+    from variable_load_5 import a
+
+    print(a)
