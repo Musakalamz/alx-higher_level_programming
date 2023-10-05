c0075a6653dded438a17b1ab54f1324205f45129
Musakalamz <ogunsolumusa@gmail.com>
2023-10-05 02:19:16 -0700

    Import a simple function from a simple file
    program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# File: 0-add.py
+# Auth: Musa_kalamz A Ogunsolu
+
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
+    a = 1 
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a,b)))
