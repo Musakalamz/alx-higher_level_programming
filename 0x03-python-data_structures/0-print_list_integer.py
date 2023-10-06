78b054db31bcf87dede9160cf89cbc29a65ee588
Musakalamz <ogunsolumusa@gmail.com>
2023-10-06 06:51:01 -0700

    Print a list of integers
    a function that prints all integers of a list.

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# File: 0-print_list_integer.py
+# Auth: Musa_kalamz A Ogunsolu
+
+
+def print_list_integer(my_list=[]):
+    """Prints all integers of a list."""
+
+    for index in range(len(my_list)):
+        print("{}".format(my_list[index]))
