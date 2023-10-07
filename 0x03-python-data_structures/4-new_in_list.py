314d8bda546edacca719870c148924448156db16
Musakalamz <ogunsolumusa@gmail.com>
2023-10-07 00:50:48 -0700

    Replace in a copy
    a function that replaces an element in a list at a specific position without modifying the original list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,9 +5,11 @@
 
 def new_in_list(my_list, idx, element):
     """Replace an element in a copied list at a specific position."""
+
+    copy = my_list.copy()
+
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list.copy())
 
-    copy = my_list.copy()
     copy[idx] = element
     return (copy)
