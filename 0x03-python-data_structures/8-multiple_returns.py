3981a26c675d2946a4dc57627640c0b3f930a779
Musakalamz <ogunsolumusa@gmail.com>
2023-10-07 01:27:31 -0700

    More returns!
    a function that returns a tuple with the length of a string and its first character.

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# File: 8-multiple_returns.py
+# Auth: Musa_kapamz A Ogunsolu
+
+
+def multiple_returns(sentence):
+    """Returns a tuple with the length of a string and its first character."""
+
+    if sentence == ():
+        sentence[0] = None
+
+    return (len(sentence), sentence[0])
