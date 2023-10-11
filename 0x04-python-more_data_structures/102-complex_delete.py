4f41873dbe86318c7767c32b5a0befbeee4be036
Musakalamz <ogunsolumusa@gmail.com>
2023-10-11 13:47:08 -0700

    Delete by value
    a function that deletes keys with a specific value in a dictionary.

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# File: 102-complex_delete.py
+# Auth: Musa_kalamz A Ogunsolu
+
+
+def complex_delete(a_dictionary, value):
+    """Delete keys with a specific value in a dictionary."""
+    while value in a_dictionary.values():
+        for k, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[k]
+                break
+
+    return (a_dictionary)
