eb13186a935f916afca66944f8ecc376b5dc3498
Musakalamz <ogunsolumusa@gmail.com>
2023-10-11 09:05:02 -0700

    Only differents
     a function that returns a set of all elements present in only one set.

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# File: 4-only_diff_elements.py
+# Auth: Musa_kalamz A Ogunsolu
+
+
+def only_diff_elements(set_1, set_2):
+    """Returns a set of all elements present in only one set."""
+    return (set_1 | set_2)
