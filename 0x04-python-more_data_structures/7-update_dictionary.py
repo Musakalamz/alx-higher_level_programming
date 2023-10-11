e0e2b6b3d2c0d274a097fbd31e10f46f2d595dc9
Musakalamz <ogunsolumusa@gmail.com>
2023-10-11 11:17:18 -0700

    Update dictionary
    a function that replaces or adds key/value in a dictionary.

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# File: 7-update_dictionary.py
+# Auth: Musa_kalamz A Ogunsolu
+
+
+def update_dictionary(a_dictionary, key, value):
+    """Replaces or adds key/value in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
