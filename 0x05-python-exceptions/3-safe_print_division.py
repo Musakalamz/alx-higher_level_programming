bd98d701fb973637db309ee7f559de9bdca5d742
Musakalamz <ogunsolumusa@gmail.com>
2023-10-23 05:57:48 -0700

    Integers division with debug
    a function that divides 2 integers and prints the result.

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# File: 3-safe_print_division.py 
+# Auth: Musa_kalamz A Ogunsolu
+
+
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
