4bd250c3f62287f94354447a9d72418be3ed4898
Musakalamz <ogunsolumusa@gmail.com>
2023-10-23 06:19:40 -0700

    Raise a message
    a function that raises a name exception with a message.

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# File: 6-raise_exception_msg.py
+# Auth: Musa_kalamz A Ogunsolu
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
