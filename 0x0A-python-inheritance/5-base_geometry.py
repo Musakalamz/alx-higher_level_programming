343d5220bb06cf2e478cf1257b8c4ef283faefb6
Musakalamz <ogunsolumusa@gmail.com>
2023-11-06 13:12:53 -0800

    Geometry module
    an empty class BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# File: 5-base_geometry.py
+# Auth: Musa_kalamz A Ogunsolu
+
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
